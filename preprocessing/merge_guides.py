882c0acb5796c65a4cd9ba8a4d948f1f3ae98161
e841018 <e841018@gmail.com>
2019-06-12 01:57:14 +0800

    group related files into preprocessing folder

--- preprocessing/merge_guides.py
+++ preprocessing/merge_guides.py
@@ -2,7 +2,6 @@ import json
 import os
 from os import listdir
 
-
 data_path = '../data/local_guides'
 
 place_list = listdir(data_path)
@@ -10,7 +9,7 @@ guides_list = {} #id:num
 threshold = 5
 for place in place_list:
     
-    f = open(os.path.join(data_path, place))
+    f = open(os.path.join(data_path, place), encoding='utf-8')
     json_data = json.load(f)
     
     for guides in json_data:
@@ -22,12 +21,7 @@ for place in place_list:
         
 guides_list = sorted(guides_list.items(), key=lambda d: d[1], reverse = True)
         
-f = open('guides.txt', 'w')
-    
-
-for i in guides_list:
-    if i[1] > threshold:
-        f.write(i[0]+'\n')
-        
-
-f.close()
+with open('guides.txt', 'w', encoding='utf-8') as f:
+    for i in guides_list:
+        if i[1] > threshold:
+            f.write(i[0]+'\n')
